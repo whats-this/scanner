c4cae1d083f6c02fbbc40c76ec907274cf67bdff
aurieh <auriteh@gmail.com>
2016-12-28 12:46:21 +0200

    Fix devdeps, freshclam

--- routes/freshclam.js
+++ routes/freshclam.js
@@ -5,12 +5,7 @@ const freshClam = require('../lib/freshclam.js');
  * > GET /freshclam
  * Run freshclam to update the virus database.
  */
-module.exports = function* freshclam() {
-  freshClam().then(() => {
-    this.status = 200;
-  }).catch(err => {
-    console.error('failed to update virus definitions');
-    console.error(err);
-    this.status = 500;
-  });
-}
+module.exports = function* freshclam () {
+  yield freshClam();
+  this.status = 200;
+};
